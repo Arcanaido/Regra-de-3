92a335cfa6e8d05fdb513b120a486a7ca3a28c79
Rafael Lins <nihillus@duck.com>
2023-09-11 00:14:21 -0300

    Update exemplo.js

--- exemplo.js
+++ exemplo.js
@@ -1,3 +1,21 @@
-/*
- * Min/max
- */
+const randomNumber = () => Math.floor(1 + Math.random() * 100)
+
+const main = (params) => {
+  const numbers = []
+  for (let i = 0; i < 100; i++) {
+    const n = randomNumber()
+    numbers.push(n)
+  }
+  let min = 200
+  let max = 0
+  numbers.forEach((n) => {
+    if (n < min) {
+      min = n
+    } 
+    if (n > max) {
+      max = n
+    }
+  })
+
+  return [min,max]
+}
