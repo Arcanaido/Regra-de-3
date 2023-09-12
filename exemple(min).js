0dcb39aead1db1ab20fa4c2d3aed006b7bcd6769
Arcanaido <132721081+Arcanaido@users.noreply.github.com>
2023-09-11 22:36:40 -0300

    Update exemple(min).js

--- exemple(min).js
+++ exemple(min).js
@@ -1,2 +1,28 @@
-// Objetico: criar um codigo que crie um array com 100 numeros randomicos inteiros e retorne o menor numero criado
-const randomNum = () => 
+
+const randomNum = () => Math.floor ( 1 + Math.random() * 100)
+
+const main = (params) => {
+  
+  const numbers = []
+
+  for ( let i = 0; i < 100; i++ ) {
+    const n = randomNum()
+    numbers.push(n)
+  }
+
+  
+let min = 200
+  
+numbers.forEach((n) => {
+
+  if ( n < min ) {
+    min = n
+  }
+})
+
+const soma = numbers.reduce ((accumulator, currentValue) => accumulator + currentValue, 0)
+
+const media = soma / numbers.length
+  
+return [min,media]
+}
